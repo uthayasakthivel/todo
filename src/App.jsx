4c75bcd0a4f25f67fbefe960207a2d94e1099409
uthayasakthivel <uthayasakthivel.work@gmail.com>
2025-02-27 23:57:39 +0530

    add functionalities and complete UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,72 @@
+import { PlusIcon, XMarkIcon } from "@heroicons/react/20/solid";
+import { useState } from "react";
+
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [todoText, setTodoText] = useState("");
+
+  const handleChange = (e) => {
+    setTodoText(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (todoText.trim() === "") return;
+    setTodoList((prevTexts) => {
+      return [...prevTexts, todoText];
+    });
+    setTodoText("");
+  };
+
+  const handleDelete = (itemIndex) => {
+    setTodoList(
+      todoList.filter((eachTodo) => {
+        return todoList.indexOf(eachTodo) !== itemIndex;
+      })
+    );
+  };
+
   return (
     <>
       <div className="bg-hero-pattern bg-cover bg-center min-h-screen flex items-center justify-center font-verdana  ">
-        <div className="p-8  max-w-md text-center rounded-xl backdrop-blur-xl backdrop-saturate-180 bg-glass-bg border border-glass-border bg-no-repeat bg-cover font-extralight ">
+        <div className="p-8  max-w-md rounded-xl backdrop-blur-xl backdrop-saturate-180 bg-glass-bg border border-glass-border bg-no-repeat bg-cover font-extralight ">
           <h2 className="text-2xl text-white">Todo Glassmorphism</h2>
-          <div>
-            <input
-              type="text"
-              placeholder="Enter your task"
-              className="mt-5 text-gray placeholder:text-slate-400  border border-slate-300 rounded-md py-2 pl-2  pr-3 shadow-sm focus:outline-none focus:border-green color-gray sm:text-sm block "
-            />
-          </div>
+          <form onSubmit={handleSubmit}>
+            <div className="mt-5 flex items-center justify-between">
+              <input
+                type="text"
+                placeholder="Enter your task"
+                onChange={handleChange}
+                value={todoText}
+                className=" text-gray placeholder:text-slate-400  border border-slate-300 rounded-md py-2 pl-2  pr-3 shadow-sm focus:outline-none focus:border-green color-gray sm:text-sm block "
+              />
+              <button type="submit">
+                <PlusIcon className="h-6 w-6 text-green cursor-pointer" />
+              </button>
+            </div>
+          </form>
+          <ul className="mt-4 flex flex-col gap-2">
+            {todoList.length === 0 ? (
+              <h3 className="text-white">No task added, Add some</h3>
+            ) : (
+              todoList.map((eachText, index) => {
+                return (
+                  <li
+                    className="p-2 rounded-md bg-glass-bg flex justify-between items-center"
+                    key={index}
+                  >
+                    <h3 className="text-white">{eachText}</h3>
+                    <XMarkIcon
+                      className="h-6 w-6 text-pink cursor-pointer "
+                      onClick={() => {
+                        handleDelete(index);
+                      }}
+                    />
+                  </li>
+                );
+              })
+            )}
+          </ul>
         </div>
       </div>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,47 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      blue: "#1fb6ff",
+      purple: "#7e5bef",
+      pink: "#ff49db",
+      orange: "#ff7849",
+      green: "#13ce66",
+      yellow: "#ffc82c",
+      "gray-dark": "#273444",
+      gray: "#8492a6",
+      "gray-light": "#d3dce6",
+      white: "#fff",
+    },
+
+    extend: {
+      fontFamily: {
+        verdana: ["Verdana"],
+      },
+      backgroundImage: {
+        "hero-pattern":
+          "url('https://images.unsplash.com/photo-1519681393784-d120267933ba?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1124&q=100')",
+      },
+      backdropBlur: {
+        xl: "16px",
+      },
+      backdropSaturate: {
+        180: "180%",
+      },
+      colors: {
+        "glass-bg": "rgba(17, 25, 40, 0.75)",
+        "glass-border": "rgba(255, 255, 255, 0.125)",
+      },
+      borderRadius: {
+        xl: "12px",
+      },
+    },
   },
   plugins: [],
 };
