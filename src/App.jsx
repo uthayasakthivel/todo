bf8e93345d5bbc5be3a4885f24f68008e9669e96
uthayasakthivel <uthayasakthivel.work@gmail.com>
2025-02-27 22:52:29 +0530

    tailwind config for glassmorphism

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="bg-hero-pattern bg-cover bg-center min-h-screen flex items-center justify-center font-verdana  ">
+        <div className="p-8  max-w-md text-center rounded-xl backdrop-blur-xl backdrop-saturate-180 bg-glass-bg border border-glass-border bg-no-repeat bg-cover font-extralight ">
+          <h2 className="text-2xl text-white">Todo Glassmorphism</h2>
+          <div>
+            <input
+              type="text"
+              placeholder="Enter your task"
+              className="mt-5 text-gray placeholder:text-slate-400  border border-slate-300 rounded-md py-2 pl-2  pr-3 shadow-sm focus:outline-none focus:border-green color-gray sm:text-sm block "
+            />
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
