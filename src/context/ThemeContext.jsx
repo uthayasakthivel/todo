6fbac76c9c1427b8880ca180659e2b61c3d307f0
uthayasakthivel <uthayasakthivel.work@gmail.com>
2025-02-28 13:27:31 +0530

    Save themeContext values on localStorage

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,15 +1,31 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 // Create Context
 export const ThemeContext = createContext();
 
 // Provider Component
 export const ThemeProvider = ({ children }) => {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(() => {
+    const savedTheme = localStorage.getItem("theme");
+    return savedTheme ? JSON.parse(savedTheme) : false;
+  });
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme) {
+      setDarkMode(JSON.parse(savedTheme));
+    }
+    console.log(darkMode);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("theme", JSON.stringify(darkMode));
+  }, [darkMode]);
 
   // Function to toggle theme
   const handleToggle = () => {
     setDarkMode(!darkMode);
+    localStorage.setItem("theme", JSON.stringify(darkMode));
   };
 
   return (
